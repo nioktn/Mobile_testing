5f3a070fb8805e9fe10bf80d626cdfe35e65df69
Nikita Nikitin <nioktn@gmail.com>
2018-08-12 13:05:08 +0300

    Created project, adden nuget packages

---
+++ RozetkaAppTesting/RozetkaLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Android.Interfaces;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+using System.Collections.Generic;
+using OpenQA.Selenium.Appium.Interfaces;
+
+namespace RozetkaLib
+{
+    public class RecyclerView
+    {
+    }
+}
---
+++ RozetkaAppTesting/RozetkaTests/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Android.Interfaces;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+using System.Collections.Generic;
+using OpenQA.Selenium.Appium.Interfaces;
+
+namespace RozetkaTests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Method()
+        {
+        }
+    }
+}
